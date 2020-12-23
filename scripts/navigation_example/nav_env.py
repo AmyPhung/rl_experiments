612b8cffd39240e15f33c776cefa85b01521c432
Amy <amyngph@gmail.com>
2020-12-23 15:59:56 -0800

    Implement rewards

--- scripts/navigation_example/nav_env.py
+++ scripts/navigation_example/nav_env.py
@@ -66,10 +66,16 @@ class NavEnv(gym.Env):
         # self.length = 0.5  # actually half the pole's length
         # self.polemass_length = (self.masspole * self.length)
         # self.force_mag = 10.0
-        self.robot_diameter = 0.1 # meters
-        self.goal_diameter = 0.3 # meters
+        self.robot_diameter = 0.3 # meters
+        self.goal_diameter = 0.4 # meters
         self.max_linear_vel = 0.4 # meters/sec
         self.max_angular_vel = 5 # radians/sec
+
+        # Rewards
+        self.goal_reward = 100000
+        self.exit_reward = -100000
+        self.time_reward = -0.7
+        self.distance_reward = 0.3
 
         # Distance at which to fail the episode
         # self.theta_threshold_radians = 12 * 2 * math.pi / 360
@@ -108,8 +114,8 @@ class NavEnv(gym.Env):
         self.seed()
         self.viewer = None
         self.state = None
-        #
-        # self.steps_beyond_done = None
+        self.steps_beyond_done = None
+        self.prev_dist = None
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -117,10 +123,10 @@ class NavEnv(gym.Env):
 
     def step(self, action):
         # Update state ---------------------------------------------------------
-        print("Previous state:")
-        print(self.state)
-        print("Action:")
-        print(action)
+        # print("Previous state:")
+        # print(self.state)
+        # print("Action:")
+        # print(action)
         err_msg = "%r (%s) invalid" % (action, type(action))
         assert self.action_space.contains(action), err_msg
 
@@ -135,50 +141,58 @@ class NavEnv(gym.Env):
 
         r_x += lin_vel*np.cos(r_theta)
         r_y += lin_vel*np.sin(r_theta)
-        print(r_theta)
-        print(lin_vel)
+        # print(r_theta)
+        # print(lin_vel)
 
         self.state = (r_x, r_y, r_theta, g_x, g_y)
-        print("Current state:")
-        print(self.state)
+        # print("Current state:")
+        # print(self.state)
 
         # Update reward --------------------------------------------------------
+        # Check if robot is within goal
+        curr_dist = np.linalg.norm([r_x-g_x, r_y-g_y])
+        within_goal = bool(curr_dist < (self.goal_diameter - self.robot_diameter))
 
-        # theta =
-        # dx =
-        # self.tau
-        #
-        # x, x_dot, theta, theta_dot = self.state
-        # force = self.force_mag if action == 1 else -self.force_mag
-        # costheta = math.cos(theta)
-        # sintheta = math.sin(theta)
-        #
-        # # For the interested reader:
-        # # https://coneural.org/florian/papers/05_cart_pole.pdf
-        # temp = (force + self.polemass_length * theta_dot ** 2 * sintheta) / self.total_mass
-        # thetaacc = (self.gravity * sintheta - costheta * temp) / (self.length * (4.0 / 3.0 - self.masspole * costheta ** 2 / self.total_mass))
-        # xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        #
-        # if self.kinematics_integrator == 'euler':
-        #     x = x + self.tau * x_dot
-        #     x_dot = x_dot + self.tau * xacc
-        #     theta = theta + self.tau * theta_dot
-        #     theta_dot = theta_dot + self.tau * thetaacc
-        # else:  # semi-implicit euler
-        #     x_dot = x_dot + self.tau * xacc
-        #     x = x + self.tau * x_dot
-        #     theta_dot = theta_dot + self.tau * thetaacc
-        #     theta = theta + self.tau * theta_dot
-        #
-        # self.state = (x, x_dot, theta, theta_dot)
-        #
-        # done = bool(
-        #     x < -self.x_threshold
-        #     or x > self.x_threshold
-        #     or theta < -self.theta_threshold_radians
-        #     or theta > self.theta_threshold_radians
-        # )
-        #
+        # Check if we left the field
+        outside_limit = bool(
+            r_x < -self.world_x_limit
+            or r_x > self.world_x_limit
+            or r_y < -self.world_x_limit
+            or r_y > self.world_x_limit)
+
+        if not within_goal and not outside_limit:
+            # We're not done yet
+            done = False
+            dist_delta = self.prev_dist - curr_dist
+            reward = self.time_reward + (dist_delta * self.distance_reward)
+
+
+        elif self.steps_beyond_done is None:
+            # We just reached a terminating case
+            done = True
+            if outside_limit:
+                reward = self.exit_reward
+                self.steps_beyond_done = 0
+
+            elif within_goal:
+                reward = self.goal_reward
+                self.steps_beyond_done = 0
+        else:
+            # We've reached a terminating case again
+            done = True
+            if self.steps_beyond_done == 0:
+                logger.warn(
+                    "You are calling 'step()' even though this "
+                    "environment has already returned done = True. You "
+                    "should always call 'reset()' once you receive 'done = "
+                    "True' -- any further steps are undefined behavior."
+                )
+            self.steps_beyond_done += 1
+            reward = 0.0
+
+        return np.array(self.state), reward, done, {}
+
+
         # if not done:
         #     reward = 1.0
         # elif self.steps_beyond_done is None:
@@ -211,8 +225,9 @@ class NavEnv(gym.Env):
                                              high=self.world_x_limit),
                       self.np_random.uniform(low=-self.world_y_limit,
                                              high=self.world_y_limit)]
-        print(self.state)
-        # self.steps_beyond_done = None
+        self.steps_beyond_done = None
+        r_x, r_y, r_theta, g_x, g_y = self.state
+        self.prev_dist = np.linalg.norm([r_x-g_x, r_y-g_y])
         return np.array(self.state)
 
     def render(self, mode='human'):
@@ -307,12 +322,15 @@ if __name__ == "__main__":
     nav_env = NavEnv()
     nav_env.reset()
 
-    nav_env.render()
+    # nav_env.render()
 
     time.sleep(1)
     sample_action = nav_env.action_space.sample()
-    nav_env.step(sample_action)
 
-    nav_env.render()
     while True:
-        pass
+        nav_env.render()
+        state, reward, done, _ = nav_env.step(sample_action)
+        print(reward)
+        if done:
+            while True:
+                pass
